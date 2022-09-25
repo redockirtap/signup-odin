e7ec051fd644154a23ddca30927ae0a40994edf3
Partrickoder <redockirtap@gmail.com>
2022-09-25 21:05:14 +0300

    Work on checkPassword() to show error message correctly.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hi');
+const form = document.querySelector('form');
+const password = form[2].value;
+const passwordToConfirm = form[6].value;
+const errorMsgField = form.querySelector('label[for="user_password"]');
+console.log(typeof errorMsgField);
+
+const checkPassword = function() {
+    const errorMsg = '(Must match and >= 8 symbols!)'
+    errorMsg.className = 'error';
+    if (form[2].value === form[6].value) errorMsgField.textContent = 'Password';
+    if (errorMsgField.textContent.match(`${errorMsg}`)) return;
+    if (form[2].value !== form[6].value) {
+        errorMsgField.textContent += ` ${errorMsg}`;
+    }
+}
+
+form.addEventListener('input', checkPassword)
