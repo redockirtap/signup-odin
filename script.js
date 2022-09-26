521e309e9008935ffb9c335a28310d0ea85d9058
Partrickoder <redockirtap@gmail.com>
2022-09-26 17:20:37 +0300

    Fix error appearing.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const form = document.querySelector('form');
-const password = form[2].value;
-const passwordToConfirm = form[6].value;
-const errorMsgField = form.querySelector('label[for="user_password"]');
+// const password = form[2].value;
+// const passwordToConfirm = form[6].value;
+const errorMsg = document.createElement('p');
+errorMsg.className = 'error';
+const errorMsgField = document.querySelector('.right-form');
 console.log(typeof errorMsgField);
 
 const checkPassword = function() {
-    const errorMsg = '(Must match and >= 8 symbols!)'
-    errorMsg.className = 'error';
-    if (form[2].value === form[6].value) errorMsgField.textContent = 'Password';
-    if (errorMsgField.textContent.match(`${errorMsg}`)) return;
+    errorMsg.textContent = 'Must match and be 8 symbols at least!';
+    if (form[2].value === form[6].value && form[2].value.length >= 8) {
+        errorMsgField.removeChild(errorMsg);
+
+    }
     if (form[2].value !== form[6].value) {
-        errorMsgField.textContent += ` ${errorMsg}`;
+        errorMsgField.appendChild(errorMsg);
     }
 }
 
